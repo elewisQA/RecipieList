3a175120771a6bc50577b73c1d10eaf720f10274
Elizabeth Lewis <elewis@academytrainee.com>
2020-10-02 10:53:39 +0100

    Initial IDE files.

---
+++ src/test/java/com/qa/recipeLists/RecipieListsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.recipeLists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipieListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
