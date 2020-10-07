0dbb7883109e8beefb9e9a86dce0c28ecdec3f05
Elizabeth Lewis <elewis@academytrainee.com>
2020-10-07 14:38:38 +0100

    Updated mapping (to-lower-case). Added CrossOrigin annotation.

--- src/main/java/com/qa/recipeLists/rest/IngredientController.java
+++ src/main/java/com/qa/recipeLists/rest/IngredientController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,8 @@ import com.qa.recipeLists.service.IngredientService;
 
 //---[ Controller Definition ]---
 @RestController
-@RequestMapping("/Ingredient") // Map to '/Ingredient' destination of URL
+@CrossOrigin
+@RequestMapping("/ingredient") // Map to '/Ingredient' destination of URL
 public class IngredientController {
 	
 	//--[ Class Variables ]--
--- src/main/java/com/qa/recipeLists/rest/StepController.java
+++ src/main/java/com/qa/recipeLists/rest/StepController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,8 @@ import com.qa.recipeLists.service.StepService;
 
 //---[ Controller Definition ]---
 @RestController
-@RequestMapping("/Step") // Map to '/step' destination of URL
+@CrossOrigin
+@RequestMapping("/step") // Map to '/step' destination of URL
 public class StepController {
 	
 	//--[ Class Variables ]--
