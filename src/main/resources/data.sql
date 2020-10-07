3688d2688a4c30dc6d3d800dde11e36085ed2baa
Elizabeth Lewis <elewis@academytrainee.com>
2020-10-07 14:36:07 +0100

    Added fake data for visual-checking of script.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO recipe(RECIPE_NAME) VALUES ('Victoria Sponge');
+INSERT INTO recipe(RECIPE_NAME) VALUES ('Shortbread');
+INSERT INTO ingredient(NAME, QUANTITY, UNIT, RECIPE_ID) VALUES ('Flour', 8, 'oz', 1);
+INSERT INTO ingredient(NAME, QUANTITY, UNIT, RECIPE_ID) VALUES ('Sugar', 8, 'oz', 1);
+INSERT INTO ingredient(NAME, QUANTITY, UNIT, RECIPE_ID) VALUES ('Butter', 8, 'oz', 1);
+INSERT INTO ingredient(NAME, QUANTITY, UNIT, RECIPE_ID) VALUES ('Eggs', 8, 'x', 1);
+INSERT INTO ingredient(NAME, QUANTITY, UNIT, RECIPE_ID) VALUES ('Vanilla Essence', 1, 'tsp', 1);
+INSERT INTO ingredient(NAME, QUANTITY, UNIT, RECIPE_ID) VALUES ('Butter', 6, 'oz', 2);
+INSERT INTO ingredient(NAME, QUANTITY, UNIT, RECIPE_ID) VALUES ('Sugar', 6, 'oz', 2);
+INSERT INTO ingredient(NAME, QUANTITY, UNIT, RECIPE_ID) VALUES ('Flour', 4, 'oz', 2);
+INSERT INTO step(DESCRIPTION, NAME, RECIPE_ID) VALUES('Combine the sugar and butter', 'Step 1', 1);
+INSERT INTO step(DESCRIPTION, NAME, RECIPE_ID) VALUES('Combine the sugar and butter', 'Step 1', 2);
+INSERT INTO step(DESCRIPTION, NAME, RECIPE_ID) VALUES('Add in the flour', 'Step 2', 1);
+INSERT INTO step(DESCRIPTION, NAME, RECIPE_ID) VALUES('Add in the flour', 'Step 2', 2);
+INSERT INTO step(DESCRIPTION, NAME, RECIPE_ID) VALUES('Add in the egg', 'Step 3', 1);
+INSERT INTO step(DESCRIPTION, NAME, RECIPE_ID) VALUES('Add in the Vanilla', 'Optional', 1);
