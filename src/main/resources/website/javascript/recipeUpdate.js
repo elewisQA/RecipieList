820c0be21f363475baeaef9dab394a060f462294
Elizabeth Lewis <elewis@academytrainee.com>
2020-10-08 11:36:36 +0100

    Update page now has all buttons, wired up with listeners.

--- src/main/resources/website/javascript/recipeUpdate.js
+++ src/main/resources/website/javascript/recipeUpdate.js
@@ -2,6 +2,11 @@
 const params = new URLSearchParams(window.location.search);
 const url = "http://localhost:1337/recipe/read/" + params.getAll('id');
 
+let id;
+let ingredients;
+let steps;
+let name;
+
 //---[ Script Body ]---
 fetch(url)
 .then(
@@ -15,19 +20,27 @@ fetch(url)
         //--[ Process Data ]--
         response.json().then(function(recipe) {
             // Set out values for easy acces
-            let id = recipe.id;
-            let ingredients = recipe.ingredients;
-            let steps = recipe.steps;
-            let name = recipe.name;
+            id = recipe.id;
+            ingredients = recipe.ingredients;
+            steps = recipe.steps;
+            name = recipe.name;
 
-            console.log(name);
-            console.log(ingredients);
-            console.log(steps);
+            console.log(recipe);
 
             // Build out the HTML
             buildTitle(id, name);
-            buildIngredientsTable(ingredients);
-            buildStepsTable(steps);
+            buildIngredientsTable();
+            buildStepsTable();
+
+            // Add Listeners to 'Add' Buttons
+            let addIngredient = document.querySelector("a#ingredient-add");
+            addIngredient.onclick = function() {
+                addIngredientRow();
+            }
+            let addStep = document.querySelector("a#step-add");
+            addStep.onclick = function() {
+                addStepRow();
+            }
         });
     }
     //--[ Handle Errors ]--
@@ -43,7 +56,7 @@ function buildTitle(id, name) {
     nameField.innerHTML = name;
 }
 
-function buildIngredientsTable(ingredients) {
+function buildIngredientsTable() {
     let tableBody = document.querySelector("tbody#ingredient-table-body");
     for (let i of ingredients) {
         // Add Ingredient Data
@@ -71,12 +84,23 @@ function buildIngredientsTable(ingredients) {
         save.setAttribute("href", "#");
         save.innerHTML = "Update";
         updateIngredientListener(save, name, qty, unit, i.id);
+
+        
+        // Add Delete Button
+        let del = document.createElement("a");
+        del.setAttribute("class", "btn btn-danger");
+        del.setAttribute("href", "#");
+        del.innerHTML = "Delete";
+        deleteListener(del, "ingredient", i.id);
+
         tr.appendChild(save);
+        tr.appendChild(del);
         tableBody.appendChild(tr);
+
     }
 }
 
-function buildStepsTable(steps) {
+function buildStepsTable() {
     let tableBody = document.querySelector("tbody#step-table-body");
     for (let s of steps) {
 
@@ -101,33 +125,117 @@ function buildStepsTable(steps) {
         save.setAttribute("href", "#");
         save.innerHTML = "Update";
         updateStepListener(save, name, description, s.id);
+
+        // Add Delete Button
+        let del = document.createElement("a");
+        del.setAttribute("class", "btn btn-danger");
+        del.setAttribute("href", "#");
+        del.innerHTML = "Delete";
+        deleteListener(del, "step", s.id);
+
         tr.appendChild(save);
+        tr.appendChild(del);
         tableBody.appendChild(tr);
-    }
-}
 
-function addNewEntry() {
-
-}
-
-function updateIngredientListener(button, name, quantity, unit, id) {
-    // Format Data from fields
-    let dataToPost = {
-        "id": id,
-        "name": name.innerHTML,
-        "unit": unit.innerHTML,
-        "quantity": quantity.innerHTML
-    }
-
-    // Send Update Request
-    button.onclick = function() {
         
-        let updateURL = "http://localhost:1337/ingredient/update/" + id;
-        console.log("URL:" + updateURL);
-        fetch(url, {
+    }
+}
+
+function addIngredientRow() {
+    let tableBody = document.querySelector("tbody#ingredient-table-body");
+    let tr = document.createElement("tr");
+    let id = document.createElement("td");
+    id.setAttribute("class", "uid");
+    id.innerHTML = "N";
+    tr.appendChild(id);
+    let name = document.createElement("td");
+    name.setAttribute("contenteditable", "true")
+    name.innerHTML = "Name";
+    tr.appendChild(name);
+    let qty = document.createElement("td");
+    qty.setAttribute("contenteditable", "true")
+    qty.innerHTML = "0";
+    tr.appendChild(qty);
+    let unit = document.createElement("td");
+    unit.setAttribute("contenteditable", "true")
+    unit.innerHTML = "x";
+    tr.appendChild(unit);
+
+    // Add 'Add' Button
+    let add = document.createElement("a");
+    add.setAttribute("class", "btn btn-warning");
+    add.setAttribute("href", "#");
+    add.innerHTML = "Add";
+    addIngredientListener(add, name, qty, unit);
+
+    // Add Delete Button
+    let del = document.createElement("a");
+    del.setAttribute("class", "btn btn-danger");
+    del.setAttribute("href", "#");
+    del.innerHTML = "Delete";
+    del.onclick = function() {
+        removeRow(tr);
+    }
+
+    tr.appendChild(add);
+    tr.appendChild(del);
+    tableBody.appendChild(tr);
+}
+
+function addStepRow() {
+    let tableBody = document.querySelector("tbody#step-table-body");
+    let tr = document.createElement("tr");
+    let id = document.createElement("td");
+    id.setAttribute("class", "uid");
+    id.innerHTML = "N";
+    tr.appendChild(id);
+    let name = document.createElement("td");
+    name.setAttribute("contenteditable", "true")
+    name.innerHTML = "Name";
+    tr.appendChild(name);
+    let desc = document.createElement("td");
+    desc.setAttribute("contenteditable", "true")
+    desc.innerHTML = "description";
+    tr.appendChild(desc);
+
+    // Add 'Add' Button
+    let add = document.createElement("a");
+    add.setAttribute("class", "btn btn-warning");
+    add.setAttribute("href", "#");
+    add.innerHTML = "Add";
+    addStepListener(add, name, desc);
+
+    // Add Delete Button
+    let del = document.createElement("a");
+    del.setAttribute("class", "btn btn-danger");
+    del.setAttribute("href", "#");
+    del.innerHTML = "Delete";
+    del.onclick = function() {
+        removeRow(tr);
+    }
+
+    tr.appendChild(add);
+    tr.appendChild(del);
+    tableBody.appendChild(tr);
+}
+
+//---[ Button Listeners ]---
+function updateIngredientListener(button, name, quantity, unit, id) {
+    let updateURL = "http://localhost:1337/ingredient/update/" + id;
+    button.onclick = function() {
+        // Format Data from fields
+        let dataToPost = {
+            "id": id,
+            "name": name.innerHTML,
+            "unit": unit.innerHTML,
+            "quantity": parseFloat(quantity.innerHTML)
+        }
+        
+        // Send Update Request
+        fetch(updateURL, {
             method: 'put',
             headers: {
-                "Content-type": "application/json"
+                "Content-type": "application/json",
             },
             body:json = JSON.stringify(dataToPost)
         })
@@ -138,20 +246,55 @@ function updateIngredientListener(button, name, quantity, unit, id) {
         .catch(function (err) {
             console.log("Request failed: ", err);
         })
+        location.reload();
     }
 }
 
-function updateStepListener(button, name, description, id) {
-    // Format Data from fields
-    let dataToPost = {
-        "id": id,
-        "name": name.innerHTML,
-        "description": description.innerHTML
-    }
-    
-    // Send Update Request
+function addIngredientListener(button, name, quantity, unit) {
+    let createURL = "http://localhost:1337/ingredient/create";
     button.onclick = function() {
-        let updateURL = "http://localhost:1337/step/update/" + id;
+        // Format Data from fields
+        let dataToPost = {
+            "name": name.innerHTML,
+            "unit": unit.innerHTML,
+            "quantity": parseFloat(quantity.innerHTML)
+        }
+
+        // Send Create Request
+        fetch(createURL, {
+            method: 'post',
+            headers: {
+                "Content-type": "application/json",
+            },
+            body:json = JSON.stringify(dataToPost)
+        })
+        .then(JSON)
+        .then(function (response) {
+            //console.log('Request succeeded with JSON response: ', data);
+            response.json().then(function(data) {
+                ingredients.push(data)
+                updateRecipe();
+            })
+        })
+        .catch(function (err) {
+            console.log("Request failed: ", err);
+        })
+
+        //TODO put returned data in Ingredients list
+    }
+}
+
+function updateStepListener(button, name, description, id) {    
+    let updateURL = "http://localhost:1337/step/update/" + id;
+    button.onclick = function() {    
+        // Format Data from fields
+        let dataToPost = {
+            "id": id,
+            "name": name.innerHTML,
+            "description": description.innerHTML
+        }
+
+        // SEnd Update Request
         console.log("URL: " + updateURL);
         fetch(updateURL, {
             method: 'put',
@@ -164,8 +307,75 @@ function updateStepListener(button, name, description, id) {
         .then(function (data) {
             console.log('Request succeeded with JSON response: ', data);
         })
+        .catch(function (error) {
+            console.log("Request failed: ", error);
+        })
+        location.reload();
+    }
+}
+
+
+function addStepListener(button, name, description) {
+    let createURL = "http://localhost:1337/step/create";
+    button.onclick = function() {
+        // Format Data from fields
+        let dataToPost = {
+            "name": name.innerHTML,
+            "description": description.innerHTML,
+        }
+
+        // Send Create Request
+        fetch(createURL, {
+            method: 'post',
+            headers: {
+                "Content-type": "application/json",
+            },
+            body:json = JSON.stringify(dataToPost)
+        })
+        .then(JSON)
+        .then(function (response) {
+            //console.log('Request succeeded with JSON response: ', data);
+            response.json().then(function(data) { 
+                steps.push(data)
+                updateRecipe();
+            })
+        })
         .catch(function (err) {
             console.log("Request failed: ", err);
         })
+
+        //TODO put returned data in Ingredients list
     }
+}
+
+function removeRow(tr) {
+    tr.parentNode.removeChild(tr);
+}
+
+function updateRecipe() {
+    let updateURL = "http://localhost:1337/recipe/update/" + id;
+    // Format Data 
+    let dataToPost = {
+        "id": id,
+        "name": name,
+        "ingredients": ingredients,
+        "steps": steps
+    }
+    console.log(dataToPost);
+
+    fetch(updateURL, {
+        method: 'put',
+        headers: {
+            "Content-type": "application/json",
+        },
+        body:json = JSON.stringify(dataToPost)
+    })
+    .then(JSON)
+    .then(function (data) {
+        console.log('Request succeeded with JSON response: ', data);
+    })
+    .catch(function (error) {
+        console.log("Request failed: ", error);
+    })
+    //location.reload();
 }
