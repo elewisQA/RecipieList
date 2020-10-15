ed42450d6b42772cc216557e646bcbd0690f7912
Elizabeth Lewis <elewis@academytrainee.com>
2020-10-15 15:42:56 +0100

    Removed unused imports, removed print-outs

--- src/main/java/com/qa/recipeLists/rest/IngredientController.java
+++ src/main/java/com/qa/recipeLists/rest/IngredientController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.recipelists.dto.IngredientDTO;
@@ -55,9 +53,6 @@ public class IngredientController {
 	
 	@PutMapping("/update/{id}")
 	public ResponseEntity<IngredientDTO> update(@PathVariable Long id, @RequestBody IngredientDTO dto) {
-		System.out.println("H\nH\nH");
-		System.out.println("ID: " + id);
-		System.out.println("Obj: " + dto.toString());
 		return new ResponseEntity<>(this.service.update(dto, id), HttpStatus.ACCEPTED);
 	}
 
